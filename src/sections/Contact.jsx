05125e267651481aff9be0f0b47713336213b1ba
Deepak_lak <dkumar17071@gmail.com>
2026-02-06 18:55:07 +0530

    completed the whole contact form

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-// import emailjs from "@emailjs/browser";
+import emailjs from "@emailjs/browser";
 
 import TitleHeader from "../components/TitleHeader";
 import ContactExperience from "../components/Models/Contact/ContactExperience";
@@ -92,7 +92,7 @@ const Contact = () => {
                   />
                 </div>
 
-                <button type="submit">
+                <button type="submit" disabled={loading}>
                   <div className="cta-button group">
                     <div className="bg-circle" />
                     <p className="text">
